95c08e9f7c431a4f46a902bad7c4aaab7a57108e
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-12-01 19:36:02 +0100

    Part 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
+struct Elf {
+    calories: Vec<i32>,
+}
+
+impl Elf {
+    fn total_calories(&self) -> i32 {
+        self.calories.iter().sum()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./input/calories").expect("Could not read file!");
+    let elves = get_elves(contents);
+    let calories: Vec<i32> = elves.iter().map(|elf| elf.total_calories()).collect();
+
+    let max_calories = calories.iter().max().unwrap();
+
+    println!("{max_calories}");
+}
+
+fn get_elves(contents: String) -> Vec<Elf> {
+    let mut elves: Vec<Elf> = Vec::new();
+    let mut calories: Vec<i32> = Vec::new();
+
+    let lines: Vec<&str> = contents.split("\n").collect();
+
+    for line in lines {
+        if line.is_empty() {
+            elves.push(Elf { calories });
+            calories = Vec::new();
+            continue;
+        }
+
+        let line = line.parse::<i32>().unwrap();
+        calories.push(line)
+    }
+
+    return elves;
 }
